2d6ed13e6ab14cfb18dbc66d5160157014087110
Benjamingitgit <beja21ae@student.cbs.dk>
2021-12-04 16:55:34 +0100

    prøver at få css til at virke

--- starter forfra/index.js
+++ starter forfra/index.js
@@ -1,4 +1,5 @@
 // her siger vi at der skal bruges express
+const { response } = require('express');
 const express = require('express');
 //app er express()
 const app = express();
@@ -6,4 +7,18 @@ const app = express();
 // Vi sætter også en console.log, så vi kan se om serveren/porten virker som den skal
 const port = 2;
 app.listen(port, ()=> console.log(`Vi online på: ${port}`));   
-sds
+
+//vi putter lige fs library på.
+fs = require('fs');
+// sætter lige sti til siderne
+const SIDERNE_AF_HTML_MEN_OGSA_JAVASCRIPT = './html pages';
+var http = require('http').Server(app);
+
+
+ 
+app.use(express.json()); //makes json 
+
+app.get('/', (req,res,next)=>{
+    return(
+    res.sendFile('./html pages/Front page.html', {root:__dirname}));
+});
